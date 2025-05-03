576957a87d57a1a25bf3921cb820990af7d99459
Fernando Vargas <fvargas1644@gmail.com>
2025-05-02 21:39:37 -0500

    Refactor the code of the openai_connection file

--- transvid/openai_connection.py
+++ transvid/openai_connection.py
@@ -1,34 +1,31 @@
 import openai
+from openai import OpenAI
 
-class OpenAIConnection:
-    def __init__(self, api_key=None):
-        self.api_key = api_key
+def create_openai_client(api_key=None):
+    """Return an OpenAI client using a provided API key or the environment's key"""
+    return OpenAI(api_key=api_key) if api_key else OpenAI()
 
-    def connection_by_local_variable(self):
-        openai.api_key = self.api_key
-        return openai
-
-    def connection_by_environment_variable(self):
-        return openai.OpenAI()    
-
-    def connection(self):
-        if self.api_key is None:
-            return self.connection_by_environment_variable()
-        else:
-            return self.connection_by_local_variable()
-
-class TextToAudioToModel(OpenAIConnection):
-    def __init__(self, input, api_key=None, model="tts-1", voice="onyx", instructions ="", response_format="mp3", speed=1):
-        super().__init__(api_key)
-        self.client = self.connection()
-        self.request_body = {
-            "input": input,
-            "model" : model,
-            "voice" : voice,
-            "instructions" : instructions,
-            "response_format" : response_format,
-            "speed" : speed,
+class TextToAudioModel:
+    def __init__(
+        self,
+        text: str,
+        api_key: str = None,
+        model: str = "tts-1",
+        voice: str = "onyx",
+        instructions: str = "",
+        response_format: str = "mp3",
+        speed: float = 1.0,
+    ):
+        self.client = create_openai_client(api_key)
+        self.params = {
+            "input": text,
+            "model": model,
+            "voice": voice,
+            "instructions": instructions,
+            "response_format": response_format,
+            "speed": speed,
         }
 
     def run_model(self):
-        return self.connection().audio.speech.create(**self.request_body)
+        print("Corriendo modelo")
+        return self.client.audio.speech.create(**self.params)

1bac729624fbd3f18615d97c4e980a585b1d50fc
Fernando Vargas <fvargas1644@gmail.com>
2025-05-05 22:26:37 -0500

    Add class to use the local whisper model

--- transvid/openai_models.py
+++ transvid/openai_models.py
@@ -1,11 +1,12 @@
 import openai
 from openai import OpenAI
+import whisper
 
 def create_openai_client(api_key=None):
     """Return an OpenAI client using a provided API key or the environment's key"""
     return OpenAI(api_key=api_key) if api_key else OpenAI()
 
-class TextToAudioModel:
+class TextToAudioModels:
     def __init__(
         self,
         text: str,
@@ -28,3 +29,12 @@ class TextToAudioModel:
 
     def run_model(self):
         return self.client.audio.speech.create(**self.params)
+
+class LocalWhisperModel:
+    def __init__(self, model="trubo"):
+        self.model = model
+        self.client = self.load_model()
+
+    def load_model(self):
+        whisper.load_model(self.model)
+

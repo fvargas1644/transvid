1f6a0a0e06bcec06b25f9f40e9146659345e237f
Fernando Vargas <fvargas1644@gmail.com>
2025-05-09 13:23:29 -0500

    Add instance variable output_name to class DownloadYoutubeVideo

--- transvid/video.py
+++ transvid/video.py
@@ -1,11 +1,18 @@
 import yt_dlp
 
 class DownloadYoutubeVideo:
-    def __init__(self, url : str, quality: str="bestvideo+bestaudio/best", format_output: str="mkv"):
+    def __init__(
+        self, 
+        url : str, 
+        quality: str="bestvideo+bestaudio/best", 
+        format_output: str="mkv", 
+        output_name : str = "video_youtube"
+    ):
         self.url = url
         self.kwargs = {
             "format": quality,
-            "merge_output_format": format_output
+            "merge_output_format": format_output,
+            "outtmpl": output_name
         }
 
     def download_video(self):

36d0f40cd9ae8ca13b46ce5b72ce1013392f1cf5
Fernando Vargas <fvargas1644@gmail.com>
2025-05-11 20:40:08 -0500

    Add FileManager class for manage the file structure

--- transvid/utils.py
+++ transvid/utils.py
@@ -21,10 +21,45 @@ def divide_text_into_parts(text : str, maximum_length : int= 13000):
 
     return text_parts
 
-def create_folder(name : str):
-    
-    folder=Path(name)
 
-    if not folder.exists(): 
-        folder.mkdir()
+class FileManager:
+    def __init__(self, main_folder : str ="transvid0"):
+        
+        # Main folder
+        self.main_folder = main_folder
+
+        # Folders
+        self.audios_folder = f"{self.main_folder}/audios"
+        self.videos_folder = f"{self.main_folder}/videos"
+        self.transcriptions_folder = f"{self.main_folder}/transcriptions"
+
+    def create_main_folder(self):
+        i = 0
+        while True:
+            folder = Path(f"transvid{i}")
+            if not folder.exists():
+                folder.mkdir()
+                self.main_folder = folder
+                break
+            i+=1
+
+    def create_folder(self, name : str):
+        folder=Path(name)
+
+        if not folder.exists(): 
+            folder.mkdir()
+
+    def create_structure(self):
+
+        # Main folder
+        self.create_main_folder()
+
+        # Folders
+        self.create_folder(self.audios_folder)
+        self.create_folder(self.videos_folder)
+        self.create_folder(self.transcriptions_folder)
+
+
+    
+
 
